98bc6ca673d7a220bf9f4f17d39ab41a283df848
Pornchai Chanaloet <kayziopro@gmail.com>
2020-10-14 17:43:52 +0200

    Pushing e03 & e04

--- e03.js
+++ e03.js
@@ -1,5 +1,13 @@
-function Marsos(str); {
-    let str = "spaceship";
-    postMessage("SOS");
-}
-console.log('Marsos')
+function Marsos(str) {
+    const sosCount = str.length / 3
+    const messages = "SOS".repeat(sosCount)
+  
+    let errorCount = 0
+    for (let i = 0; i < str.length; i++) {
+      if (str[i] !== messages[i]) {
+        errorCount++
+      }
+    }
+  
+    return errorCount
+  }
